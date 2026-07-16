7b2acdfdbcee38e57e38ad583c99108eb4f731c3
Sukhpreet's MacBook <sukhpreetsmacbook@Sukhpreetss-MacBook-Air.local>
2026-07-16 00:43:48 -0500

    Product to Dashboard and Ransack functions added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  def self.ransackable_associations(auth_object = nil)
+    ["category_id"]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["category_id", "created_at", "id", "name", "price", "updated_at"]
+  end
+
 end
