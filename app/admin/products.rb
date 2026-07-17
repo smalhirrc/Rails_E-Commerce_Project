f8a08e4be436a8a41f71ec1696e7c3df57554398
Sukhpreet's MacBook <sukhpreetsmacbook@Sukhpreetss-MacBook-Air.local>
2026-07-17 07:23:43 -0500

    Images associated to products

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -4,7 +4,63 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :category_id, :name, :price
+  permit_params :name, :price, :category_id, :image
+
+  preserve_default_filters!
+  remove_filter :image_attachment
+  remove_filter :image_blob
+
+  # Image display in product row
+  index do
+    selectable_column
+    id_column
+    column :name 
+    column :category do |product|
+      product.category&.name || "Uncategorized"
+    end
+    column :price do |product|
+      number_to_currency(product.price)
+    end
+    column :image do |product|
+      if product.image.attached?
+        image_tag url_for(product.image), class: "admin-thumb", style: "width: 50px; height: auto;"
+      else
+        "No Image"
+      end
+    end
+    actions
+  end
+
+  # Form field for image
+  form html: { multipart: true } do |f|
+    f.inputs "Product Details" do
+      f.input :category, as: :select, collection: Category.all.map { |c| [c.name, c.id] }, include_blank: "Select Category"
+      f.input :name
+      f.input :price
+      f.input :image, as: :file, hint: f.object.image.attached? ? image_tag(url_for(f.object.image), style: "width: 100px; height: auto;") : "No image uploaded yet"
+    end
+    f.actions
+  end
+
+  # Successfully uploaded image show
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :category
+      row :price do |product|
+        number_to_currency(product.price)
+      end
+      row :image do |product|
+        if product.image.attached?
+          image_tag url_for(product.image), style: "max-width: 300px; height: auto;"
+        else
+          "No image uploaded"
+        end
+      end
+    end
+    active_admin_comments
+  end
   #
   # or
   #
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :category
 
-  validates :title, presence: true
+  validates :name, presence: true
+
+  has_one_attached :image
+  # has_many_attached :images
 
   def self.ransackable_associations(auth_object = nil)
     [ "category_id" ]
