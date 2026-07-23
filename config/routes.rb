05d24c7d9e326d043182de991846b38fec60ebf4
Sukhpreet's MacBook <sukhpreetsmacbook@Sukhpreetss-MacBook-Air.local>
2026-07-23 01:46:15 -0500

    Navigate through categories

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "/about", to: "pages#show", defaults: { slug: "about" }
   get "/contact", to: "pages#show", defaults: { slug: "contact" }
+
+  resources :categories
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
