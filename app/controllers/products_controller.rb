cee4a32a3bab48f432474885160cf5844af24961
Sukhpreet's MacBook <sukhpreetsmacbook@Sukhpreetss-MacBook-Air.local>
2026-07-24 01:32:52 -0500

    Search available products by keywords or category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,19 @@
 class ProductsController < ApplicationController
+    def index
+        @products = Product.includes(:category, image_attachment: :blob)
+
+        if params[:search].present?
+            @products = @products.where("products.name LIKE ?", "%#{params[:search]}%")
+        end
+
+        if params[:category_id].present?
+            @products = @products.where("products.category_id = ?", params[:category_id])
+            @category = Category.find(params[:category_id])
+        end
+
+        @products = @products.page(params[:page]).per(4)
+    end
+
     def show
         @product = Product.includes(:category, image_attachment: :blob).find(params[:id])
     end
