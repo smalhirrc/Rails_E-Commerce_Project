b62206254db71a9c18f0c60b08869eed03de58e8
Sukhpreet's MacBook <sukhpreetsmacbook@Sukhpreetss-MacBook-Air.local>
2026-07-30 04:50:54 -0500

    Sign up form created

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,9 @@ Rails.application.routes.draw do
   post "/cart/checkout", to: "carts#checkout"
   get "/cart/order/:id", to: "carts#order"
 
+  get "/signup", to: "home#signup"
+  post "/signup", to: "home#signup"
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
